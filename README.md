72fb4c7f6e8b9f5adbe14027bbd529d40e45beec
maxbeizer <max.beizer@gmail.com>
2018-11-20 18:06:02 -0600

    Add working pass

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# Elm project
+# Testing out Elm + Graphql
+Reading along with [this blog post](https://medium.com/@zenitram.oiram/a-beginners-guide-to-graphql-with-elm-315b580f0aad)
+Get a personal access token and grep for access token and replace references
+
+## Other useful commands
+`elm reactor`
+`npm run api`
 
 ## Getting started
 
-You need to have [Elm](http://elm-lang.org/) 0.18 installed on your machine.
+You need to have [Elm](http://elm-lang.org/) 0.19 installed on your machine.
 
 Compile this project with:
 
--- src/Main.elm
+++ src/Main.elm
@@ -1,67 +1,100 @@
-import Html exposing (..)
---import Html.Attributes exposing (..)
---import Html.Events exposing (onClick)
+module Main exposing (main)
+
+import Browser
+import Github.Object
+import Github.Object.User as User
+import Github.Query as Query
+import Graphql.Http
+import Graphql.Operation exposing (RootQuery)
+import Graphql.SelectionSet exposing (SelectionSet, with)
+import Html exposing (Html, a, div, h1, h2, p, pre, text)
+import RemoteData exposing (RemoteData)
 
 
-main : Program Never Model Msg
-main =
-    Html.program
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+query : SelectionSet Response RootQuery
+query =
+    Query.selection Response
+        |> with (Query.user { login = "octocat" } user)
+
+
+user : SelectionSet User Github.Object.User
+user =
+    User.selection User
+        |> with User.name
+
+
+type alias Response =
+    { user : Maybe User }
+
+
+type alias User =
+    { name : Maybe String }
 
 
 
--- MODEL
+-- Get your GitHub bearer access token from https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 
 
-type alias Model =
-    {}
-
-
-initialModel : Model
-initialModel =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.none )
-
-
-
--- UPDATE
+makeRequest : Cmd Msg
+makeRequest =
+    query
+        |> Graphql.Http.queryRequest
+            "https://api.github.com/graphql"
+        |> Graphql.Http.withHeader "authorization" "Bearer <YOUR GITHUB BEARER TOKEN>"
+        |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
 
 
 type Msg
-    = NoOp
+    = GotResponse Model
+
+
+type alias Model =
+    RemoteData (Graphql.Http.Error Response) Response
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( RemoteData.Loading
+    , makeRequest
+    )
+
+
+view : Model -> Html.Html Msg
+view model =
+    div []
+        [ case model of
+            RemoteData.Loading ->
+                text "Loading"
+
+            RemoteData.Failure e ->
+                text <| "Something went wrong: " ++ Debug.toString e
+
+            RemoteData.NotAsked ->
+                text "Request has not been made yet"
+
+            RemoteData.Success response ->
+                case response.user of
+                    Just ghUser ->
+                        div []
+                            [ h2 [] [ text (Maybe.withDefault "User has no name" ghUser.name) ] ]
+
+                    Nothing ->
+                        text "No user was found with that name"
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        GotResponse response ->
+            ( response, Cmd.none )
 
 
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text "Hello, world!"
-        , text (toString model)
-        ]
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
